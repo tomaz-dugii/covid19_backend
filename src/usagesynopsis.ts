dc2abdc443c40b09089439daa457a5aa172fdd3a
gcink <cinkcink@gmail.com>
2020-11-30 12:49:08 +0100

    extra spaces

--- src/usagesynopsis.ts
+++ src/usagesynopsis.ts
@@ -2,7 +2,7 @@ import {Request, Response} from 'express'
 import {db} from './db'
 
 export async function usagesynopsis(req: Request, res: Response) {
-    return res.send('Usage synopsis: <br><br>\    
+    return res.send('Usage synopsis: <br><br>\
         * list covid per day: <br>\
             - http://localhost:3000/covidPerday <br>\
         <br>\
